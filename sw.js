a0de5cc90e89719536a64628e9c3b0247689fc3e
ZhaoCheng <zhoachengzcq@gmail.com>
2019-03-25 11:44:21 +0800

    no message

--- sw.js
+++ sw.js
@@ -1,7 +1,36 @@
 self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open('v1').then(cache => {
-      return cache.addAll(['/zcq/', '/zcq/index.html', '/zcq/index.js'])
+      return cache.addAll([
+        '/zcq/',
+        '/zcq/index.html',
+        '/zcq/index.js',
+        '/zcq/error.html',
+        '/zcq/img/img_404error.png'
+      ])
     })
+  )
+})
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches
+      .match(event.request)
+      .then(response => {
+        if (response !== undefined) {
+          console.log('caches.match() message is:' + response)
+          return response
+        } else {
+          return fetch(event.request).then(response => {
+            let responseClone = response.clone()
+            caches.open('v1').then(cache => {
+              cache.put(event.request, responseClone)
+            })
+            return response
+          })
+        }
+      })
+      .catch(() => {
+        return caches.match('/error.html')
+      })
   )
 })
